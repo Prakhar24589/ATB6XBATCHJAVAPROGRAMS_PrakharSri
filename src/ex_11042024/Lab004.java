72b940ed14766677bda79fc255d8ddf7eb12c08a
My Name <myEmail@example.com>
2024-04-18 14:37:05 +0530

    Initial commit

---
+++ src/ex_11042024/Lab004.java
@@ -0,0 +1,12 @@
+package ex_11042024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        int age =65;
+        age=70;
+        System.out.println(age);
+    }
+}
+
+//integer type of container storing 65 as value in the variable name =age.
+// equal to(=) is used to assign value
---
+++ src/ex_11042024/Lab005.java
@@ -0,0 +1,13 @@
+package ex_11042024;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        int age = 1;
+        age=-1;
+        age=age+1;
+        System.out.println(age);
+
+
+
+    }
+}
